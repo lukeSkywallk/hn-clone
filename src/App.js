d15f1862f0fb3af2569f12c122c628776c9c454c
edilson <edilson.w3g@gmail.com>
2019-10-06 19:01:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import hackerNewsApi from './api/hackerNewsApi.js';
 
-function App() {
-  return (
+export default function App() {
+
+  useEffect(() => {
+    async function getData(){
+      hackerNewsApi.getStory(1);
+    }
+
+    getData();
+    
+  },[]);
+
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-export default App;
