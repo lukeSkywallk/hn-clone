e68212e2eadb856e0d4622c50bf9deaad6d96f68
ejesus <disys.edilson.jesus@segurossura.com.br>
2019-10-08 07:34:38 -0300

    structuring project

--- src/components/story.js
+++ src/components/story.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useFetch } from '../api/hooks';
 
 export default function Story({storyId}){
+    const BASE_URL = 'https://hacker-news.firebaseio.com/v0/';
+    const SUFFIX_URL = '.json?print=pretty';
+    const [urlApi, setUrlApi] = useState(undefined);
+    const [data, loading] = useFetch(urlApi);
+
+    useEffect(() =>{
+        setUrlApi(`${BASE_URL}/item/${storyId}${SUFFIX_URL}`)
+    },[setUrlApi, storyId]);
+
+
+
+//    https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty
     return(
         <div>
-            {storyId}
+            {
+            loading ? ("Carregando...")
+            :
+            (data.url)
+            }
         </div>
     );
 }
